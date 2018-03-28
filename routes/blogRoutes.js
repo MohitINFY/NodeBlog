fbc7797f1f176f4aa2efa0d42aacb8eb71db6599
Mohit Bansal <mohit.bansal@ad.infosys.com>
2018-03-28 23:01:21 +0530

    Initial commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,30 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
 
+    // Create the Redi Cache
+    const util = require('util');
+    const redis = require('redis');
+    const redisURL = 'redis://127.0.0.1:6379';
+
+    //Create the Redis Client
+    const client = redis.createClient(redisURL);
+
+    //Promisif the Client.Get function () 
+    client.get = util.promisify(client.get);
+
+    // Get the Blogs based on User ID from the Cache
+    const cachedBlogs = await client.get(req.user.id);
+    
+    if(cachedBlogs) {
+      console.log("Serving from CACHE");
+      return res.send(cachedBlogs);
+    }
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log("Seving from Mongo DB");
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
---
+++ services/cache.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+const redisURL = 'redis://127.0.0.1:6379';
+
+const client = redis.createClient(redisURL);
+
+client.get - util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    })
+    return exec.apply(this, arguments);
+}
