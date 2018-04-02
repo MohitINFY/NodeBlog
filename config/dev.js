6d4ab1152ac52ed262e191489e2913305ec0f34f
Mohit Bansal <mohit.bansal@ad.infosys.com>
2018-04-02 11:26:17 +0530

    test

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://mohit:mohit@ds123799.mlab.com:23799/blog_dev',
+  mongoURI: 'mongodb://mohit:mohit@ds047075.mlab.com:47075/blog_dev',
   cookieKey: '123123123'
 };
