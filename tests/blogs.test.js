f40f6ee4281f818522df7d298750fa38f3838b19
Mohit Bansal <mohit.bansal@ad.infosys.com>
2018-04-02 11:51:59 +0530

    test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -22,4 +22,18 @@ describe('when logged in', async () => {
         const label = await page.getContentsOf('form label');
         expect(label).toEqual('Blog Title');
     }, 30000)
+
+    describe('when login, test invalid inputs', async ()=> {
+
+        beforeEach(async ()=> {
+            await page.click('form button');
+        });
+        
+        test('the form shows an Error message', async ()=> {
+          const titleError = await page.getContentsOf('.title .red-text');
+          const contentError = await page.getContentsOf('.content .red-text');
+          expect(titleError).toEqual('You must provide a value');
+          expect(contentError).toEqual('You must provide a value');
+        });
+    });
 })
