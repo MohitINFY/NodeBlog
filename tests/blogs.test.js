b325260b9fc5895bcbfb212609b3bec0898fab91
Mohit Bansal <mohit.bansal@ad.infosys.com>
2018-04-02 12:07:15 +0530

    in Progress

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -23,12 +23,34 @@ describe('when logged in', async () => {
         expect(label).toEqual('Blog Title');
     }, 30000)
 
-    describe('when login, test invalid inputs', async ()=> {
 
+    describe('And using valid inputs', async ()=> {
+
+       beforeEach(async ()=> {
+          await page.type('.title input', 'My Title');
+          await page.type('.content input', 'My Content');
+          await page.click('form button');
+       }) 
+
+       test('submiting takes to User Screen', async ()=> {
+          const text = page.getContentsOf('h5');
+          expect(text).toEqual('Please confirm your entries');
+       });
+
+       test('Final submit takes back to Index Screen', async ()=> {
+           await page.click('button.green');
+           await this.page.waitFor('.card');
+           const title = page.getContentsOf('.card-title');
+           expect(title).toEqual('My title');
+       });
+
+    })
+
+    describe('when login, test invalid inputs', async ()=> {
         beforeEach(async ()=> {
             await page.click('form button');
         });
-        
+
         test('the form shows an Error message', async ()=> {
           const titleError = await page.getContentsOf('.title .red-text');
           const contentError = await page.getContentsOf('.content .red-text');
