c42b9bd1ad1feba57d5e60f8154b2a330cbdf378
Mohit Bansal <mohit.bansal@ad.infosys.com>
2018-04-14 01:06:46 +0530

    test

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    '784170941110-ldfb8nl7l2icr62aqaq6u4a4v3oi3cd5.apps.googleusercontent.com',
+  googleClientSecret: 'oTOjmV4aLUXR65kJWlEIF6ek',
   mongoURI: 'mongodb://mohit:mohit@ds047075.mlab.com:47075/blog_dev',
   cookieKey: '123123123'
 };
